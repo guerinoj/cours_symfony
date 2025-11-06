0b2d6f8f3150edcc2c1592b4a4cecd003bdb3910
guerinoj <guerino.jonathan@gmail.com>
2025-11-06 14:52:13 +0100

    feat: remplacer les articles codés en dur par des articles récupérés depuis le dépôt dans le composant Navbar

--- src/Twig/Components/Navbar.php
+++ src/Twig/Components/Navbar.php
@@ -2,19 +2,28 @@
 
 namespace App\Twig\Components;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 final class Navbar
 {
+
+  public function __construct(private PostRepository $postRepository) {}
+
   public array $links = [
     ['path' => 'home.index', 'label' => 'Home'],
     ['path' => 'actu.index', 'label' => 'Last news'],
   ];
 
-  public array $news = [
-    ['slug' => 'article-1', 'title' => 'Article 1'],
-    ['slug' => 'article-2', 'title' => 'Article 2'],
-    ['slug' => 'article-3', 'title' => 'Article 3'],
-  ];
+
+  public function getPosts(): array
+  {
+    return $this->postRepository->findBy(
+      ['is_published' => true],
+      ['createdAt' => 'DESC'],
+      5
+    );
+  }
 }
