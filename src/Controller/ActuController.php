f0f1ddc22a37580581430f02e2cf3c74d1550bb1
guerinoj <guerino.jonathan@gmail.com>
2025-11-06 16:02:07 +0100

    feat: ajouter les méthodes d'édition et de suppression des articles dans ActuController

--- src/Controller/ActuController.php
+++ src/Controller/ActuController.php
@@ -16,8 +16,10 @@ final class ActuController extends AbstractController
     #[Route('/actu/create', name: 'actu.create')]
     public function create(EntityManagerInterface $entityManager): Response
     {
+        $postsCount = $entityManager->getRepository(Post::class)->count([]);
+
         $post = new Post();
-        $post->setTitle('Article inconnu');
+        $post->setTitle('Article Symfony' . ($postsCount + 1));
         $post->setContent('Content of the new article');
         $post->setCreatedAt(new \DateTimeImmutable());
         $post->setIsPublished(true);
@@ -46,6 +48,46 @@ final class ActuController extends AbstractController
         return $this->render('actu/show.html.twig', [
             'post' => $post,
         ]);
+    }
+
+    #[Route('/actu/{id}/edit', name: 'actu.edit', requirements: ['id' => '\d+'])]
+    public function edit(PostRepository $postRepository, int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Find the post by its ID
+        $post = $postRepository->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        // Update the post (for example, change the title)
+        $post->setTitle('Symfony troisième essai');
+        $post->setUpdatedAt(new \DateTimeImmutable());
+        // Persist and flush the changes here
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+
+        // Redirect to post view after editing
+        return $this->redirectToRoute('actu.show', ['id' => $post->getId()]);
+    }
+
+    #[Route('/actu/{id}/delete', name: 'actu.delete', requirements: ['id' => '\d+'])]
+    public function delete(PostRepository $postRepository, int $id, EntityManagerInterface $entityManager): Response
+    {
+        // Find the post by its ID
+        $post = $postRepository->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Article not found');
+        }
+
+        // Remove the post
+        $entityManager->remove($post);
+        $entityManager->flush();
+
+        // Redirect to the index after deletion
+        return $this->redirectToRoute('actu.index');
     }
 
     #[Route('/actu', name: 'actu.index')]
