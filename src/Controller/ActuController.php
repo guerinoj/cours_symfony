2f9b810ef27800e26a36924ee056aa0e9f5bbb1c
guerinoj <guerino.jonathan@gmail.com>
2025-11-06 12:04:38 +0100

    feat: ajouter la méthode de création d'article dans ActuController

--- src/Controller/ActuController.php
+++ src/Controller/ActuController.php
@@ -2,12 +2,35 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Dom\Entity;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ActuController extends AbstractController
 {
+
+    #[Route('/actu/create', name: 'actu.create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+        $post = new Post();
+        $post->setTitle('New Article');
+        $post->setContent('Content of the new article');
+        $post->setCreatedAt(new \DateTimeImmutable());
+        $post->setIsPublished(true);
+        $post->setAuthor('John Doe');
+
+        // Persist the new post entity
+        $entityManager->persist($post);
+
+        // Flush to save it to the database
+        $entityManager->flush();
+
+        return new Response('Create an article : ' . $post->getTitle());
+    }
+
     #[Route('/actu/{slug}', name: 'actu.show', requirements: ['slug' => '[a-z0-9\-]+'])]
     public function show(string $slug): Response
     {
