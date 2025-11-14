c3686655717bb7cddb146409f00f5ee93bfb7e6e
guerinoj <guerino.jonathan@gmail.com>
2025-11-14 11:18:34 +0100

    feat: ajouter des liens dynamiques dans la barre de navigation pour les utilisateurs connectés et les administrateurs

--- src/Twig/Components/Navbar.php
+++ src/Twig/Components/Navbar.php
@@ -3,19 +3,38 @@
 namespace App\Twig\Components;
 
 use App\Repository\PostRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 final class Navbar
 {
 
-  public function __construct(private PostRepository $postRepository) {}
+  public function __construct(
+    private PostRepository $postRepository,
+    private Security $security
+  ) {}
 
-  public array $links = [
-    ['path' => 'home.index', 'label' => 'Home'],
-    ['path' => 'actu.index', 'label' => 'Last news'],
-  ];
+  public function getLinks(): array
+  {
+    $links = [
+      ['path' => 'home.index', 'label' => 'Accueil', 'icon' => '🏠'],
+      ['path' => 'actu.index', 'label' => 'Actualités', 'icon' => '📰'],
+    ];
 
+    // Ajouter des liens pour les utilisateurs connectés
+    if ($this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
+      $links[] = ['path' => 'app_category_index', 'label' => 'Catégories', 'icon' => '📁'];
+      $links[] = ['path' => 'author.index', 'label' => 'Auteurs', 'icon' => '✍️'];
+    }
+
+    // Ajouter des liens admin si l'utilisateur a le rôle ROLE_ADMIN
+    if ($this->security->isGranted('ROLE_ADMIN')) {
+      $links[] = ['path' => 'app_category_new', 'label' => 'Nouvelle catégorie', 'icon' => '➕'];
+    }
+
+    return $links;
+  }
 
   public function getPosts(): array
   {
